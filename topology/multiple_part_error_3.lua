3c6c9996eaa3fbafa92857f6a91e25a88abda858
Pablo Strasser <strassp6@etu.unige.ch>
2013-02-05 15:05:32 +0100

    Added Conclusion
    Added Introduction
    Added Jet
    Added missing file for the multi particle example
    Changed title

---
+++ topology/multiple_part_error_3.lua
@@ -0,0 +1,25 @@
+
+tex.sprint('\\begin{tikzpicture}[x=3cm,y=3cm]')
+for i=0,1 do
+  for j=0,1 do
+    colfluid=''
+    if j==0 or j==1 then
+      colfluid='fill=blue!10'
+    end
+    color1='blue'
+    color2='blue'
+    if i==1 then
+      color1='red'
+    end
+    if j==1 then
+      color2='red'
+    end
+      tex.sprint('\\draw['..colfluid..'] ('..i..','..j..') rectangle ('..(i+1)..','..(j+1)..');')
+    if i==1 and j==1 then
+      tex.sprint('\\draw[red,->] ('..(i+0.5)..','..(j-0.1)..') -- ('..(i+0.5)..','..(j+0.1)..') ; ')
+    end
+   end
+end
+tex.sprint('\\node[circle,fill,label=below:1] at(0.78,1.67){};')
+tex.sprint('\\node[circle,fill,label=below:2] at(1.3,1.4){};')
+tex.sprint('\\end{tikzpicture}')
